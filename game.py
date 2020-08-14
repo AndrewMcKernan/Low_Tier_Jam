6d80830f062b9701bbf7167735ebf98fb214aec1
Andrew McKernan <andrewmck96@gmail.com>
2020-08-13 22:53:30 -0600

    attacks, movement, collision, life
    Created logic for enemies moving toward the player
    Created attack animations
    Allowed enemies to be killed by player attacks
    Spawned enemies in one of 4 corners of the map
    Added heart indicator for player health
    Allowed enemies to damage player by contact

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame
 from sprites import *
 from colours import Colour
 import time
+from random import randrange
 
 
 def game_loop():
@@ -9,6 +10,9 @@ def game_loop():
     width = 500
     height = 500
     fps = 30
+    max_enemies = 10
+    ticks_between_spawns = 15
+    ticks_since_last_spawn = 0
 
     pygame.init()
     pygame.mixer.init()  # for sound
@@ -16,15 +20,21 @@ def game_loop():
     pygame.display.set_caption("Survive")
 
     enemies = pygame.sprite.Group()
+    life = pygame.sprite.Group()
     all_sprites = pygame.sprite.Group()
 
     clock = pygame.time.Clock()
 
     spider = Enemy()
     enemies.add(spider)
+
     all_sprites.add(spider)
     player = Player()
     all_sprites.add(player)
+    for i in range(player.health):
+        life_sprite = Life(i)
+        all_sprites.add(life_sprite)
+        life.add(life_sprite)
     game_running = True
     while game_running:
         # start = time.time()
@@ -34,7 +44,8 @@ def game_loop():
             if event.type == pygame.QUIT:
                 game_running = False
         keys = pygame.key.get_pressed()
-
+        if keys[pygame.K_SPACE]:
+            player.begin_attack()
         if keys[pygame.K_LEFT] or keys[pygame.K_a]:
             player.rect.x -= player.velocity
         if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
@@ -44,8 +55,22 @@ def game_loop():
         if keys[pygame.K_DOWN] or keys[pygame.K_s]:
             player.rect.y += player.velocity
         # update stuff
-        enemies.update()
+        enemies.update(player)
         player.update(enemies)
+        life.update(player.health)
+        if len(enemies) < max_enemies and ticks_since_last_spawn > ticks_between_spawns:
+            baddie = Enemy()
+            # spawn in one of the 4 corners of the map
+            x = randrange(2)
+            y = randrange(2)
+            baddie.rect.x = width * x
+            baddie.rect.y = height * y
+            enemies.add(baddie)
+            all_sprites.add(baddie)
+            ticks_since_last_spawn = 0
+
+        if ticks_since_last_spawn <= ticks_between_spawns:
+            ticks_since_last_spawn += 1
         # render
         screen.fill(Colour.BLACK)
         all_sprites.draw(screen)
--- sprites.py
+++ sprites.py
@@ -4,29 +4,70 @@ from image_loader import get_image
 
 
 class Player(pygame.sprite.Sprite):
-    velocity = 10
+    velocity = 6
     old_x = 250
     old_y = 250
     facing_right = False
     old_facing_right = False
+    attack_frame = 0
+    attacking = True
+    consistent_dimensions = (26, 22)
+    scale = 3
+    health = 3
+    invincibility_frames = 60
+    current_invincibility_frame = 0
+    is_invincible = False
+    attack_cooldown = 15
+    time_since_last_attack = 0
 
     def __init__(self):
         # Call the parent class (Sprite) constructor
         pygame.sprite.Sprite.__init__(self)
 
         original_dimensions = (14, 20)
-        scale = 5
+        # scale = 5
+        # consistent_dimensions = (26,22)
 
         # Create an image of the block, and fill it with a color.
         # This could also be an image loaded from the disk.
-        self.image = get_image('char_standing.png', tuple(i * scale for i in original_dimensions))
+        self.image = get_image('char_standing_sized.png', tuple(i * self.scale for i in self.consistent_dimensions))
 
         # Fetch the rectangle object that has the dimensions of the image
         # Update the position of this object by setting the values of rect.x and rect.y
         self.rect = self.image.get_rect()
         self.rect.center = (500 / 2, 500 / 2)
 
+    def begin_attack(self):
+        if not self.attacking and self.time_since_last_attack == 0:
+            self.attacking = True
+
+    def attack(self):
+        if self.attack_frame >= 5:
+            self.attack_frame = 0
+            self.attacking = False
+            return False
+        self.attack_frame += 1
+        self.time_since_last_attack += 1
+        self.image = get_image('attack_' + str(self.attack_frame) + '_sized.png',
+                               tuple(i * self.scale for i in self.consistent_dimensions))
+        return True
+
     def update(self, *args):
+        if self.is_invincible:
+            self.current_invincibility_frame += 1
+            if self.current_invincibility_frame > self.invincibility_frames:
+                self.is_invincible = False
+                self.current_invincibility_frame = 0
+        if self.attacking:
+            still_attacking = self.attack()
+            if self.facing_right and still_attacking:
+                # flip horizontally (True) not vertically (False)
+                self.image = pygame.transform.flip(self.image, True, False)
+        if 0 < self.time_since_last_attack < self.attack_cooldown:
+            self.time_since_last_attack += 1
+        elif self.time_since_last_attack >= self.attack_cooldown:
+            self.time_since_last_attack = 0
+
         # args contains the list of enemy sprites. If we collide with them, don't allow us to move into them
         enemies_collided = pygame.sprite.spritecollide(self, args[0], False)
         for enemy in enemies_collided:
@@ -54,13 +95,14 @@ class Player(pygame.sprite.Sprite):
         self.old_x = self.rect.x
         self.old_y = self.rect.y
         if self.facing_right is not self.old_facing_right:
+            # flip horizontally (True) not vertically (False)
             self.image = pygame.transform.flip(self.image, True, False)
             self.old_facing_right = self.facing_right
 
 
 class Enemy(pygame.sprite.Sprite):
 
-    velocity = 10
+    velocity = 2
     old_x = 400
     old_y = 400
     facing_right = False
@@ -71,7 +113,7 @@ class Enemy(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
 
         original_dimensions = (17, 11)
-        scale = 5
+        scale = 3
 
         # Create an image of the block, and fill it with a color.
         # This could also be an image loaded from the disk.
@@ -83,6 +125,45 @@ class Enemy(pygame.sprite.Sprite):
         self.rect.center = (400, 400)
 
     def update(self, *args):
+        # args[0] contains the player sprite, which allows us to know his position and figure out what to do
+        player_sprite = args[0]
+
+        # first, move however we need to move
+        # TODO: don't let the velocity allow the x/y coord to exceed the player's current position to prevent jittering
+        if player_sprite.rect.x > self.rect.x:
+            self.rect.x += self.velocity
+        elif player_sprite.rect.x < self.rect.x:
+            self.rect.x -= self.velocity
+        if player_sprite.rect.y > self.rect.y:
+            self.rect.y += self.velocity
+        elif player_sprite.rect.y < self.rect.y:
+            self.rect.y -= self.velocity
+        # then, handle the collision
+        collided = pygame.sprite.collide_rect(self, player_sprite)
+        if collided:
+            if player_sprite.attacking:
+                self.kill()
+                return
+            elif not player_sprite.is_invincible:
+                player_sprite.health -= 1
+                player_sprite.is_invincible = True
+            if player_sprite.rect.x > self.rect.x:
+                # we collided to the right, don't move right
+                if self.rect.x > self.old_x:
+                    self.rect.x = self.old_x
+            elif player_sprite.rect.x < self.rect.x:
+                # we collided to the left, don't move left
+                if self.rect.x < self.old_x:
+                    self.rect.x = self.old_x
+            if player_sprite.rect.y > self.rect.y:
+                # we collided to the top, don't move down
+                if self.rect.y > self.old_y:
+                    self.rect.y = self.old_y
+            elif player_sprite.rect.y < self.rect.y:
+                # we collided to the bottom, don't move up
+                if self.rect.y < self.old_y:
+                    self.rect.y = self.old_y
+
         if self.old_x < self.rect.x:
             # we are moving to the right, so perform a horizontal flip
             self.facing_right = True
@@ -93,3 +174,42 @@ class Enemy(pygame.sprite.Sprite):
         if self.facing_right is not self.old_facing_right:
             self.image = pygame.transform.flip(self.image, True, False)
             self.old_facing_right = self.facing_right
+
+        if self.old_x < self.rect.x:
+            # we are moving to the right, so perform a horizontal flip
+            self.facing_right = True
+        elif self.old_x > self.rect.x:
+            self.facing_right = False
+        self.old_x = self.rect.x
+        self.old_y = self.rect.y
+        if self.facing_right is not self.old_facing_right:
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.old_facing_right = self.facing_right
+
+
+class Life(pygame.sprite.Sprite):
+
+    life_number = None
+
+    def __init__(self, life_number):
+        # Call the parent class (Sprite) constructor
+        pygame.sprite.Sprite.__init__(self)
+        self.life_number = life_number
+        original_dimensions = (26, 22)
+        scale = 1
+
+        # Create an image of the block, and fill it with a color.
+        # This could also be an image loaded from the disk.
+        self.image = get_image('life.png', tuple(i * scale for i in original_dimensions))
+
+        # Fetch the rectangle object that has the dimensions of the image
+        # Update the position of this object by setting the values of rect.x and rect.y
+        self.rect = self.image.get_rect()
+        print(original_dimensions[0] * life_number)
+        self.rect.x = original_dimensions[0] * life_number * 2
+        self.rect.y = 0
+
+    def update(self, *args):
+        # args[0] is the health of the player
+        if args[0] - 1 < self.life_number:
+            self.kill()
